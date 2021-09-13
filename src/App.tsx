3f7d1047c1c871cd52d45410d1b826eec566d43a
Arthur Yan <arthuryan08@gmail.com>
2021-09-12 23:05:47 -0300

    Header Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Header } from './components/Header';
 import { Login } from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-     <Login/>
+     <Header />
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React from "react";
 import Logo from "../assets/images/logo.png";
 
 import "../styles/Header.scss";
 
-export function Header(){
-    return(
-        <header className="header">
-        <img src={Logo} alt="Artflix logo" />
-      </header>
-    )
+export function Header() {
+  return (
+    <header className="header">
+      <img src={Logo} alt="Artflix logo" />
+      <ul className='menu-list'>
+        <li>
+          <a href="#">Início</a>
+        </li>
+        <li>
+          <a href="#">Séries</a>
+        </li>
+        <li>
+          <a href="#">Filmes</a>
+        </li>
+        <li>
+          <a href="#">Mais Recentes</a>
+        </li>
+        <li>
+          <a href="#">Minha Lista</a>
+        </li>
+      </ul>
+    </header>
+  );
 }
