204a9fd5a42a35b834945a0a5d0336e9856f3c2f
Arthur Yan <arthuryan08@gmail.com>
2021-09-13 15:34:34 -0300

    Header Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Header } from './components/Header';
+import { Home } from './pages/Home';
 import { Login } from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-     <Header />
+     <Home />
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,37 @@
 import React from "react";
 import { Header } from "../components/Header";
+import HeroTitle from "../assets/images/diabo-de-cada-dia.png";
+import Top10 from "../assets/images/badge-top-10.png";
+import "../styles/Home.scss";
+import { BiPlay, BiInfoCircle } from "react-icons/bi";
 
-export function Home(){
-    return(
-        <div>
-            <Header />
+export function Home() {
+  return (
+    <div>
+      <Header />
+      <div className="hero-capa">
+        <div className="hero-infos">
+          <img src={HeroTitle} alt="Titulo do filme" />
+          <div className="top1">
+            <img src={Top10} alt="Top 10" />
+            <h1>Top 1 de hoje no Brasil</h1>
+          </div>
+          <p>
+            Pregadores Profanos. Autoridades Corruptas. Amantes Assassinos. Numa
+            cidade cheia de pecadores, um jovem busca justiça.
+          </p>
+          <div className="btn">
+            <button>
+              <BiPlay />
+              Assistir
+            </button>
+            <button className="btn-info">
+              <BiInfoCircle />
+              Mais Informações
+            </button>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
