65b784925d684302d1cba2a1f84a8128ebe9121a
Arthur Yan <arthuryan08@gmail.com>
2021-09-15 21:20:14 -0300

    Component Modal finalized

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import HeroCapa from "../assets/images/capa-filme.png";
 import HeroNome from "../assets/images/diabo-de-cada-dia.png";
-import { AiFillPlayCircle, AiOutlinePlusCircle } from "react-icons/ai";
+import { AiFillPlayCircle } from "react-icons/ai";
 import { HiThumbUp, HiThumbDown } from "react-icons/hi";
-import '../styles/Modal.scss'
+import "../styles/Modal.scss";
 import { BiPlay } from "react-icons/bi";
 
 export function Modal() {
   return (
-    <div className="fundao">
     <div className="modal">
       <div className="modal-content">
         <div className="modal-hero">
           <img src={HeroNome} alt="nome" />
           <div className="btn-hero">
-            <button><BiPlay />Assistir</button>
+            <button>
+              <BiPlay />
+              Assistir
+            </button>
             <span>
               <a href="#">
                 <AiFillPlayCircle />
@@ -33,26 +35,98 @@ export function Modal() {
           </div>
         </div>
         <div className="modal-infos">
-          <section>
-            <p>
-              Pregadores Profanos. Autoridades Corruptas. Amantes Assassinos. Numa
-              cidade cheia de pecadores, um jovem busca justiça.
-            </p>
-          </section>
-          <aside>
-            <p>
-              Elenco:<text> Arthur Yan, Roxana Meireles, Tom Holland, Tobey Maguire.</text>
-            </p>
-            <p>
-              Gênero:<text> Ação, Romance, Terror.</text>
-            </p>
-            <p>
-              Cenas e momentos:<text> Violento.</text>
-            </p>
-          </aside>
+          <div className="description">
+            <section>
+              <p>
+                Pregadores Profanos. Autoridades Corruptas. Amantes Assassinos.
+                Numa cidade cheia de pecadores, um jovem busca justiça.
+              </p>
+            </section>
+            <aside>
+              <p>
+                Elenco:
+                <text>
+                  {" "}
+                  Arthur Yan, Roxana Meireles, Tom Holland, Tobey Maguire.
+                </text>
+              </p>
+              <p>
+                Gênero:<text> Ação, Romance, Terror.</text>
+              </p>
+              <p>
+                Cenas e momentos:<text> Violento.</text>
+              </p>
+            </aside>
+          </div>
+          <div className="modal-eps">
+            <div className="ep-selector">
+              <h1>Episódios</h1>
+              <select>
+                <option value="1">Temporada 1</option>
+                <option value="2">Temporada 2</option>
+                <option value="3">Temporada 3</option>
+              </select>
+            </div>
+            <ul>
+              <li>
+                <h1>1</h1>
+                <img src={HeroCapa} alt="ícone do episódio" />
+                <div>
+                  <h3>Título primeiro</h3>
+                  <span>45min</span>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident inventore quae nihil eum odio laborum!
+                  </p>
+                </div>
+              </li>
+              <li>
+                <h1>1</h1>
+                <img src={HeroCapa} alt="ícone do episódio" />
+                <div>
+                  <h3>Título primeiro</h3>
+                  <span>45min</span>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident inventore quae nihil eum odio laborum!
+                  </p>
+                </div>
+              </li>
+              <li>
+                <h1>1</h1>
+                <img src={HeroCapa} alt="ícone do episódio" />
+                <div>
+                  <h3>Título primeiro</h3>
+                  <span>45min</span>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident inventore quae nihil eum odio laborum!
+                  </p>
+                </div>
+              </li>
+              <li>
+                <h1>1</h1>
+                <img src={HeroCapa} alt="ícone do episódio" />
+                <div>
+                  <h3>Título primeiro</h3>
+                  <span>45min</span>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident inventore quae nihil eum odio laborum!
+                  </p>
+                </div>
+              </li>
+              <li>
+                <h1>1</h1>
+                <img src={HeroCapa} alt="ícone do episódio" />
+                <div>
+                  <h3>Título primeiro</h3>
+                  <span>45min</span>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident inventore quae nihil eum odio laborum!
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
